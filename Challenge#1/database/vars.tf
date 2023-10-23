26e20403d38759883309e78a88dfefc1a753f3ff
Anustha Singh <10145642+anustha-singh@users.noreply.github.com>
2023-10-23 18:50:57 +0530

    Create vars.tf

---
+++ Challenge#1/database/vars.tf
@@ -0,0 +1,6 @@
+variable "database" {}
+variable "resource_group" {}
+variable "location" {}
+variable "database_version" {}
+variable "database_admin_user" {}
+variable "database_admin_password" {}
