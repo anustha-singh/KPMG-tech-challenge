42f5ae16024e395749d16b9b8cfdae8965ccffe7
Anustha Singh <10145642+anustha-singh@users.noreply.github.com>
2023-10-23 21:33:27 +0530

    Create vars.tf

---
+++ Challenge#1/vars.tf
@@ -0,0 +1,10 @@
+variable "resource_group" {}
+variable "location" {}
+variable "be_subnet_id" {}
+variable "fe_subnet_id" {}
+variable "be_host_name"{}
+variable "be_username" {}
+variable "be_os_password" {}
+variable "fe_host_name"{}
+variable "fe_username" {}
+variable "fe_os_password" {}
