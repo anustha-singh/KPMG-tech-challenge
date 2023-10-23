ef3bba9eba91211467baf0735c9c0cea9ec51fac
Anustha Singh <10145642+anustha-singh@users.noreply.github.com>
2023-10-23 18:48:26 +0530

    Update main.tf

--- Challenge#1/database/main.tf
+++ Challenge#1/database/main.tf
@@ -1,1 +1,15 @@
+resource "azurerm_sql_server" "primary" {
+    name = var.primary_database
+    resource_group_name = var.resource_group
+    location = var.location
+    version = var.database_version
+    administrator_login = var.database_admin_user
+    administrator_login_password = var.database_admin_password
+}
 
+resource "azurerm_sql_database" "db" {
+  name                = "db"
+  resource_group_name = var.resource_group
+  location            = var.location
+  server_name         = azurerm_sql_server.primary.name
+}
